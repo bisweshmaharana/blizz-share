91a5dc8c2ed543d41f0754836a9e4583b857b373
Biswesh Maharana <bisweshmaharana@gmail.com>
2025-03-12 05:36:42 +0530

    Fix: Update build configuration for Render deployment

--- next.config.js
+++ next.config.js
@@ -1,15 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: {
-    serverActions: true,
-  },
   env: {
     APP_URL: process.env.NODE_ENV === 'development' ? 'http://localhost:3002' : process.env.APP_URL,
   },
   swcMinify: true,
   images: {
     domains: ['localhost', 'res.cloudinary.com'],
+  },
+  webpack: (config) => {
+    config.resolve.fallback = { fs: false, path: false };
+    return config;
   },
 };
 
